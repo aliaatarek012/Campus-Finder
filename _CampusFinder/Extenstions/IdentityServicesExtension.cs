5740834cd4abfbe451f7aa7eab949fa5f5954710
AliaaTarek <aliaa.tarek@gmail.com>
2025-03-13 23:57:32 +0200

    Auth Module Creation

---
+++ _CampusFinder/Extenstions/IdentityServicesExtension.cs
@@ -0,0 +1,69 @@
+﻿using _CampusFinderCore.Entities.Identity;
+using _CampusFinderCore.Services.Contract;
+using _CampusFinderInfrastructure.Identity;
+using _CampusFinderService;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace _CampusFinder.Extenstions
+{
+    public static class IdentityServicesExtension
+    {
+
+        //Extension Method to IServiceCollection
+        public static IServiceCollection AddIdentityServices(this IServiceCollection services, IConfiguration configuration)
+        {
+
+            services.AddScoped(typeof(IAuthService), typeof(AuthService));
+
+            services.AddIdentity<AppUser, IdentityRole>(options =>
+            {
+
+                options.SignIn.RequireConfirmedAccount = false;
+                options.SignIn.RequireConfirmedEmail = true;
+                options.Password.RequireDigit = true;
+                options.Password.RequiredLength = 6;
+                options.Password.RequireNonAlphanumeric = true;
+                options.Password.RequireUppercase = true;
+                options.Password.RequireLowercase = true;
+
+                options.Lockout.AllowedForNewUsers = true;
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+                options.Lockout.MaxFailedAccessAttempts = 5;
+
+
+            }).AddEntityFrameworkStores<AppIdentityDbContext>().AddDefaultTokenProviders();
+
+            services.AddJwtBearerConfiguration(configuration); // If using JWT
+            return services;
+        }
+
+        private static void AddJwtBearerConfiguration(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateAudience = true,
+                    ValidAudience = configuration["JWT:ValidAudience"],
+                    ValidateIssuer = true,
+                    ValidIssuer = configuration["JWT:ValidIssuer"],
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT:AuthKey"])),
+                    ValidateLifetime = true,
+                    ClockSkew = TimeSpan.FromDays(double.Parse(configuration["JWT:DurationInDays"]))
+                };
+            });
+
+
+        }
+
+    }
+}
